8ed6df62b160fd8529e1652721403fd8017dd946
Joje <nemesienjoachim@gmail.com>
2021-03-13 17:30:13 +0100

    New tests

--- src/test/java/books/BookShopTest.java
+++ src/test/java/books/BookShopTest.java
@@ -19,4 +19,25 @@ public class BookShopTest {
         int[] Basket = {4,2,1,1,1};
         assertEquals(58,Shop.cost(Basket));
     }
+
+    @Test
+    public void mySecondTest(){
+        BookShop Shop = new BookShop("BookShop");
+        int[] Basket = {1,2,1,1,0};
+        assertEquals(31.04,Shop.cost(Basket));
+    }
+
+    @Test
+    public void myThirdTest(){
+        BookShop Shop = new BookShop("BookShop");
+        int[] Basket = {1,1,1,0,0};
+        assertEquals(20.64,Shop.cost(Basket));
+    }
+
+    @Test
+    public void myFourthTest(){
+        BookShop Shop = new BookShop("BookShop");
+        int[] Basket = {5,2,0,0,0};
+        assertEquals(54.88,Shop.cost(Basket));
+    }
 }
